45fd47a8cd73e2691f6f18c719d0a119a3c3955c
horatiu <horatiu.cod@gmail.com>
2024-01-05 18:02:49 +0200

    add ConfigureAuthenticateSchemeOptions implement IConfigureOptions<AuthenticationOptions>

---
+++ src/Keycloak.Net.Authentication/ConfigureAuthenticateSchemeOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+
+
+namespace Keycloak.Net.Authentication;
+
+internal class ConfigureAuthenticateSchemeOptions : IConfigureOptions<AuthenticationOptions>
+{
+    public void Configure(AuthenticationOptions options)
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+}
