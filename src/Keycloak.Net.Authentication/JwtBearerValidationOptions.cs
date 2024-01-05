7be49bcf56e6ac206b34590d9fd1bc1f0b3d2a10
horatiu <horatiu.cod@gmail.com>
2024-01-05 17:51:59 +0200

    add JwtBearerValidationOptions and TokenValidationParametersOptions classes

---
+++ src/Keycloak.Net.Authentication/JwtBearerValidationOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+
+namespace Keycloak.Net.Authentication;
+
+public class JwtBearerValidationOptions : JwtBearerOptions
+{
+    [Required]
+    public new string Authority { get; set; } = string.Empty;
+    [Required]
+    public string ClientId { get; set; } = string.Empty;
+    public string NameClaim { get; set; } = "preferred_username";
+    public new TokenValidationParametersOptions TokenValidationParameters { get; set; } = new TokenValidationParametersOptions();
+}
+
+public class TokenValidationParametersOptions : TokenValidationParameters
+{
+    [Required]
+    public new string ValidAudience { get; set; } = string.Empty;
+    [Required]
+    public new string ValidIssuer { get; set; } = string.Empty;
+    public new bool ValidateIssuerSigningKey { get; set; } = true;
+}
