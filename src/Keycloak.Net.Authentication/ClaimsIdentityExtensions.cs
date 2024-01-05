5455a88afdd450819d8c7a7790dfb3819b666f42
horatiu <horatiu.cod@gmail.com>
2024-01-05 17:59:15 +0200

    get required claim

---
+++ src/Keycloak.Net.Authentication/ClaimsIdentityExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace Keycloak.Net.Authentication;
+
+internal static class ClaimsIdentityExtensions
+{
+    public static bool TryGetClaim(this ClaimsIdentity claimsIdentity, Predicate<Claim> claim, out Claim? returnClaim)
+    {
+        if (claimsIdentity.HasClaim(claim))
+        {
+            returnClaim = claimsIdentity.FindFirst(claim);
+            return true;
+        }
+        returnClaim =null;
+        return false;
+    }
+}
