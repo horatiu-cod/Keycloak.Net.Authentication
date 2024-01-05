5d3366eb4263f28ff6275195047708bdb1c63cbd
horatiu <horatiu.cod@gmail.com>
2024-01-05 17:57:51 +0200

    map keycloak roles to asp.net identity roles

---
+++ src/Keycloak.Net.Authentication/TokenRoleMapper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace Keycloak.Net.Authentication;
+
+internal static class TokenRoleMapper
+{
+    private static readonly Predicate<Claim> ResourceAccessAdapter = claim => claim.Type == "resource_access";
+    private static readonly string RoleKeyName = "roles";
+
+    public static string[] GetRoles(ClaimsIdentity identity, string clientId)
+    {
+        var returnRoles = Array.Empty<string>();
+
+        if (identity.TryGetClaim(ResourceAccessAdapter, out var resourseAccessClaim)) 
+        {
+            if (resourseAccessClaim is not null)
+            {
+                var resourceAccess = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string[]>>>(resourseAccessClaim.Value);
+                if (resourceAccess is not null && resourceAccess.TryGetValue(clientId, out var clientResourceAccess) && clientResourceAccess.TryGetValue(RoleKeyName, out var roles))
+                {
+                    returnRoles = roles;
+                }
+            }
+        }
+        return returnRoles;
+    }
+}
