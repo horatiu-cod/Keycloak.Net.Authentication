c1d3138dd4efc45756778618028684f9380ac358
horatiu <horatiu.cod@gmail.com>
2024-01-05 18:03:32 +0200

    add ConfigureJwtBearerValidationOptions, implement IConfigureNamedOptions<JwtBearerOptions>

---
+++ src/Keycloak.Net.Authentication/ConfigureJwtBearerValidationOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+namespace Keycloak.Net.Authentication;
+
+internal class ConfigureJwtBearerValidationOptions : IConfigureNamedOptions<JwtBearerOptions>
+{
+    private IOptions<JwtBearerValidationOptions> JwtBearerValidationOptions { get; }
+
+    public ConfigureJwtBearerValidationOptions(IOptions<JwtBearerValidationOptions> jwtBearerValidationOptions)
+    {
+        JwtBearerValidationOptions = jwtBearerValidationOptions;
+    }
+
+    public void Configure(string? name, JwtBearerOptions options)
+    {
+        if (string.Equals(name, JwtBearerDefaults.AuthenticationScheme))
+        {
+            Configure(options);
+        }
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        options = Mapper.MapFromJwtBearerValidationOptions(JwtBearerValidationOptions.Value);
+
+        options.TokenValidationParameters = Mapper.MapFromTokenValidationParametersOptions(JwtBearerValidationOptions.Value);
+    }
+}
