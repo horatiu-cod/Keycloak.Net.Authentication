6d530d851f605ace88f5cdd90a10d98ab710bcf5
horatiu <horatiu.cod@gmail.com>
2024-01-08 11:32:22 +0200

    added

---
+++ samples/Keycloak.Net.Authentication.Api/Controllers/SampleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Keycloak.Net.Authentication.Api.Controllers
+{
+
+    [Route("api/")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("authenticate")]
+        [Authorize]
+        public IActionResult CheckAuthorization()
+        {
+            return Ok($"{HttpStatusCode.OK} authenticated");
+        }
+        [HttpGet("authorize")]
+        [Authorize(Roles = "user_role")]
+        public IActionResult GetAuthorization() => 
+              Ok($"{HttpStatusCode.OK} authorized");
+    }
+}
