f45152a624102f572be6c6ae392218509ce4d4e0
horatiu <horatiu.cod@gmail.com>
2024-01-11 10:10:32 +0200

    add GetAudience() , returns available audience

--- src/Keycloak.Net.Authentication/Utils/KeycloakClaimsTransformation.cs
+++ src/Keycloak.Net.Authentication/Utils/KeycloakClaimsTransformation.cs
@@ -1,4 +1,5 @@
-﻿using Keycloak.Net.Authentication.Extensions;
+﻿using Keycloak.Net.Authentication.Common;
+using Keycloak.Net.Authentication.Extensions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
@@ -7,13 +8,18 @@ namespace Keycloak.Net.Authentication.Utils;
 
 internal class KeycloakClaimsTransformation : IClaimsTransformation
 {
-    private IOptions<JwtBearerValidationOptions>? JwtOptions { get; }
+    private IOptions<JwtBearerValidationOptions> JwtOptions { get; }
 
-    public KeycloakClaimsTransformation(IOptions<JwtBearerValidationOptions>? jwtOptions)
+    public KeycloakClaimsTransformation(IOptions<JwtBearerValidationOptions> jwtOptions)
     {
         JwtOptions = jwtOptions;
     }
-
+    /// <summary>
+    /// Map and transform the keycloak jwt realm and client "roles" claim to identity "role" claim
+    /// Map and transform the keycloak jwt "preferred_username" claim to identity "name" claim
+    /// </summary>
+    /// <param name="principal"></param>
+    /// <returns>Task of System.Security.Claims.ClaimsPrincipal</returns>
     public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
         var claimsIdentity = (ClaimsIdentity?)principal.Identity;
@@ -26,29 +32,38 @@ internal class KeycloakClaimsTransformation : IClaimsTransformation
     }
     private bool ShouldTransform(ClaimsIdentity? claimsIdentity)
     {
-        if (claimsIdentity is not null && claimsIdentity.HasClaim(c => c.Type == "role"))
+        if (claimsIdentity is not null && claimsIdentity.HasClaim(c => c.Type == Constants.RoleClaimType))
         {
             return false;
         }
-        if (claimsIdentity is not null && claimsIdentity.TryGetClaim(c => c.Type == "iss", out var issClaim) && issClaim is not null && JwtOptions is not null)
+        if (claimsIdentity is not null && claimsIdentity.TryGetClaim(c => c.Type == Constants.IssuerClaimType, out var issClaim) && issClaim is not null && JwtOptions is not null)
         {
             if (string.IsNullOrEmpty(JwtOptions.Value.Authority))
-                return string.Equals(issClaim.Value, JwtOptions.Value.ValidIssuer, StringComparison.OrdinalIgnoreCase);
+                return (string.Equals(issClaim.Value, JwtOptions.Value.ValidIssuer, StringComparison.OrdinalIgnoreCase) || string.Equals(issClaim.Value, JwtOptions.Value.ValidIssuers.Where(c => c == issClaim.Value).FirstOrDefault(), StringComparison.OrdinalIgnoreCase));
             return string.Equals(issClaim.Value, JwtOptions.Value.Authority, StringComparison.OrdinalIgnoreCase);
         }
         return false;
     }
+    /// <summary>
+    /// Map and transform the keycloak jwt "roles" claim to identity "role" claim type
+    /// </summary>
+    /// <param name="claimsIdentity"></param>
     private void MapRoleClaim(ClaimsIdentity? claimsIdentity)
     {
-        if (JwtOptions is not null && JwtOptions.Value.ClientId is not null && claimsIdentity is not null)
+        var audience = GetAudience();
+        if (JwtOptions is not null && claimsIdentity is not null)
         {
-            var roles = TokenRoleMapper.GetRoles(claimsIdentity, JwtOptions.Value.ClientId);
+            var roles = TokenRoleMapper.GetRoles(claimsIdentity, audience);
             foreach (var role in roles)
             {
                 claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
             }
         }
     }
+    /// <summary>
+    /// Map and transform the keycloak jwt "preferred_username" claim to identity "name" claim type
+    /// </summary>
+    /// <param name="claimsIdentity"></param>
     private void MapNameClaim(ClaimsIdentity? claimsIdentity)
     {
         if (JwtOptions is not null && claimsIdentity is not null)
@@ -61,5 +76,15 @@ internal class KeycloakClaimsTransformation : IClaimsTransformation
                 return;
             }
         return;
+    }
+    private string GetAudience()
+    {
+        if (!string.IsNullOrEmpty(JwtOptions.Value.Audience))
+            return JwtOptions.Value.Audience!;
+        if (!string.IsNullOrEmpty(JwtOptions.Value.ValidAudience))
+            return JwtOptions.Value.ValidAudience!;
+        if (JwtOptions.Value.ValidAudiences.Any())
+            return JwtOptions.Value.ValidAudiences.FirstOrDefault()!;
+        return string.Empty;
     }
 }
