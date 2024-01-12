69e8a5ff5f6c18e6a33a9d105642f6a1231910e9
horatiu <horatiu.cod@gmail.com>
2024-01-12 18:56:08 +0200

    add unit test

---
+++ tests/UnitTest/Keycloak.Net.Authentication.Test/JWT/TokenRoleMapperTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Keycloak.Net.Authentication.JWT;
+using System.Security.Claims;
+
+namespace Keycloak.Net.Authentication.Test.JWT;
+#pragma warning disable
+public class TokenRoleMapperTest : IClassFixture<ClaimIdentityFixture>
+{
+    ClaimIdentityFixture _fixture;
+
+    public TokenRoleMapperTest(ClaimIdentityFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void TokenRoleMapper_GetRoles_ShouldReturnListOfRoles()
+    {
+        //Arrange
+        var claimsIdentity =(ClaimsIdentity?)_fixture.SetClaimsIdentity.Identity;
+
+        //Act
+        var roles = TokenRoleMapper.GetRoles(claimsIdentity, "audience");
+
+        //Assert
+        roles.Count().Should().Be(4);
+        roles.Should().Contain("client_first_role");
+        roles.Should().Contain("client_second_role");
+        roles.Should().Contain("realm_first_role");
+        roles.Should().Contain("realm_second_role");
+
+    }
+}
