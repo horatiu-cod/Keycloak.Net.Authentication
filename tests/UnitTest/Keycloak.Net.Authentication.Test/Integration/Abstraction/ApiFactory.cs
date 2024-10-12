f700810b70de2a6c900d0c4864dfbd0aad05fc96
horatiu <horatiu.cod@gmail.com>
2024-10-12 21:00:36 +0300

    refactor testcontainer

--- tests/UnitTest/Keycloak.Net.Authentication.Test/Integration/Abstraction/ApiFactory.cs
+++ tests/UnitTest/Keycloak.Net.Authentication.Test/Integration/Abstraction/ApiFactory.cs
@@ -8,20 +8,18 @@ namespace Keycloak.Net.Authentication.Test.Integration.Abstraction;
 public class ApiFactory : WebApplicationFactory<IApiMarker> , IAsyncLifetime
 {
     public string? BaseAddress { get; set; } = "https://localhost:8843";
-    //public string? BaseAddress { get; set; }
 
 
     private readonly KeycloakContainer _container = new KeycloakBuilder()
         .WithImage("keycloak/keycloak:24.0")
         .WithPortBinding(8843, 8443)
-        .WithEnvironment("KC_HTTP_ENABLED", "false")
         .WithResourceMapping("./Integration/import/oidc.json", "/opt/keycloak/data/import")
         .WithResourceMapping("./Integration/Certs/localhostcert.pem", @"/opt/keycloak/certs")
         .WithResourceMapping("./Integration/Certs/localhostkey.pem", @"/opt/keycloak/certs")
         .WithEnvironment(@"KC_HTTPS_CERTIFICATE_FILE", @"/opt/keycloak/certs/localhostcert.pem")
         .WithEnvironment(@"KC_HTTPS_CERTIFICATE_KEY_FILE", @"/opt/keycloak/certs/localhostkey.pem")
         .WithCommand("--import-realm")
-        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8080))
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(8443))
         .Build();
 
     public async Task InitializeAsync()
